93cb71ab616cf36a0da15b25d659288c8e0c7ab2
MdShariyar56 <shahriyarkobir2005@gmail.com>
2025-09-25 11:44:10 +0600

    Added a setup navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,31 @@ import React from 'react';
 const App = () => {
   return (
     <>
-          <div class="navbar bg-base-100 shadow-sm flex justify-around items-center">
-          <a class="btn btn-ghost text-xl ml-8">CS — Ticket SYSTEM</a>
-  {/* বাম পাশের অংশ: লোগো এবং মোবাইলের জন্য টোগল মেনু */}
-  <div>
-    <div class="dropdown">
-      <div tabIndex={0} role="button" class="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+      <div className="navbar bg-base-100 shadow-sm">
+  <div className="navbar-start">
+    <a className="btn btn-ghost text-xl">CS — Ticket System</a>
+  </div>
+
+  <div className="navbar-end">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" 
+             viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" 
+                d="M4 6h16M4 12h8m-8 6h16" />
         </svg>
       </div>
-      <ul tabIndex={0} class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Link</a></li>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
         <li><a>Home</a></li>
         <li><a>FAQ</a></li>
         <li><a>Changelog</a></li>
         <li><a>Blog</a></li>
         <li><a>Download</a></li>
         <li><a>Contact</a></li>
-        <li><a class="btn btn-primary">+ New Ticket</a></li>
       </ul>
     </div>
-    
-  </div>
 
-  {/* ডান পাশের অংশ: ডেস্কটপের জন্য সম্পূর্ণ মেনু */}
-  <div class="hidden lg:flex">
-    <ul class="menu menu-horizontal px-10">
-      <li><a>Link</a></li>
+    <ul className="menu menu-horizontal px-1 hidden lg:flex">
       <li><a>Home</a></li>
       <li><a>FAQ</a></li>
       <li><a>Changelog</a></li>
@@ -38,9 +35,10 @@ const App = () => {
       <li><a>Download</a></li>
       <li><a>Contact</a></li>
     </ul>
-    <a class="btn btn-primary">+ New Ticket</a>
+    <a className="btn  bg-gradient-to-r from-[#632ee3cc]  to-[#9f62f2cc]">+ New Ticket</a>
   </div>
 </div>
+
     </>
   );
 };
